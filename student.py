5efcfe4099ff7594f93b2367f7d0c66a528ae7ba
Laxmi-81 <153051815+Laxmi-81@users.noreply.github.com>
2024-06-26 08:14:38 +0530

    student.py

--- student.py
+++ student.py
@@ -1,13 +1,27 @@
 class Student:
-  students=dict()
-  n=int(input("enter the number of students:"))
-  for i in range(n):
-      name=input("enter the name of the student:")
-      reg=int(input("enter register number:"))
-      marks=[]
-      for j in range(n):
-         mark=int(input("enter marks:"))
-         marks.append(mark)
-         students[name]=reg,marks
-  print(students)
-Student()
+    def __init__(self, name, usn, m1,m2):
+        self.name = name
+        self.usn = usn
+        self.m1 = m1
+        self.m2 = m2
+
+
+    def __str__(self):
+        return f"Name: {self.name}, USN: {self.usn}, Mark1: {self.m1},Mark2: {self.m2}"
+
+st_dict = {}
+
+num = int(input("Enter the number of student:"))
+
+for i in range(num):
+    name = input(f"Enter name for student {i+1}: ")
+    age = input(f"Enter usn for student {i+1}: ")
+    m1 = input(f"Enter marks of student subject 1: ")
+    m2 = input(f"Enter marks of student subject 2: ")
+
+    student = Student(name, age, m1,m2)
+    st_dict[f"Student {i+1}"] = student
+
+print("\nStudents are :")
+for key, student in st_dict.items():
+    print(f"{key}: {student}")
